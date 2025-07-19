32e9771e490964b38d593f16ac62982757d0ca6d
quyansiyuanwang <2301179475@qq.com>
2025-07-19 23:08:02 +0800

    refactor: Cancel dynamic import Executors

--- src/WorkflowEngine/Executors/__init__.py
+++ src/WorkflowEngine/Executors/__init__.py
@@ -1,1 +1,6 @@
-from . import *
+from .InputExecutor import InputExecutor
+from .OCRExecutor import OCRExecutor
+from .ROIExecutor import ROIExecutor
+from .SystemExecutor import SystemExecutor
+
+__all__ = ["InputExecutor", "OCRExecutor", "ROIExecutor", "SystemExecutor"]
--- src/WorkflowEngine/__init__.py
+++ src/WorkflowEngine/__init__.py
@@ -1,10 +1,8 @@
 from typing import List
 
 from .executor import Executor, ExecutorManager, JobExecutor
+from .Executors import *
 from .manager import WorkflowManager
-
-# init built-in Executors
-JobExecutor.import_trigger("src.WorkflowEngine.Executors")
 
 __all__: List[str] = [
     # executor
